131bc14a33f2fcff31337476d75951a0bc8948be
KylerMarkle <kyler.markle@gmail.com>
2019-06-05 21:11:29 -0700

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 6.0f;
+    private float height = 5.0f;
+
+    private float rotateSpeed = 2.0f;
+
+    private const float distance_max = 12.0f;
+    private const float distance_min = 3.0f;
+
+    private const float height_max = 8.0f;
+    private const float height_min = 3.5f;
+    public float currentX = 0.0f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    public void Update()
+    {
+        currentX += Input.GetAxis("Horizontal Rotation") * rotateSpeed;
+
+        distance += Input.GetAxis("Zoom");
+        height += Input.GetAxis("Zoom") / 2;
+
+        height = Mathf.Clamp(height, height_min, height_max);
+        distance = Mathf.Clamp(distance, distance_min, distance_max);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, height, -distance);
+
+        Quaternion rotation = Quaternion.Euler(0, currentX, 0);
+
+        camTransform.position = lookAt.position + rotation * dir;
+
+        camTransform.LookAt(lookAt.position);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Movement VAR's
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Rigidbody rigid;
+    public Vector3 Direction;
+
+    Animator mAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Animation();
+        Movement();
+    }
+
+    void Animation()
+    {
+        mAnim = GameObject.FindWithTag("Player").GetComponent<Animator>();
+
+        mAnim.SetFloat("Speed", Math.Max(Math.Abs(Input.GetAxis("Horizontal")), Math.Abs(Input.GetAxis("Vertical"))));
+    }
+
+    void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 jump = new Vector3(0, jumpForce, 0);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(jump, ForceMode.Impulse);
+        }
+
+        if (moveHorizontal + moveVertical != 0)
+        {
+            rigid.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.2F);
+        }
+
+        Vector3 nextpos = transform.position;
+
+        nextpos += movement * moveSpeed * Time.deltaTime;
+
+        rigid.MovePosition(nextpos);
+    }
+}
+
